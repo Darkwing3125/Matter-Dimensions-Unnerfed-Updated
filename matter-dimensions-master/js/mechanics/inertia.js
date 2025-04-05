0a10c9bb91147c4899faeac18b7e83a57755f4b9
Darkwing3125 <thedarklord2317@gmail.com>
2025-04-05 10:49:31 +0800

    Update inertia.js

--- matter-dimensions-master/js/mechanics/inertia.js
+++ matter-dimensions-master/js/mechanics/inertia.js
@@ -1,9 +1,9 @@
 function inertia_gain_speed() {
-    return big(1).div(player.best_time_speed).pow(player.upgrades['INERTIA_2'].get_effect()).mult(player.upgrades['INERTIA_4'].get_effect());
+    return big(1).div(player.best_time_speed).pow(player.upgrades['INERTIA_2'].get_effect()).mult(player.upgrades['INERTIA_4'].get_effect()).mult(big(player.inertia_multiplier));
 }
 
 function inertia_loss_speed() {
-    return big(2).pow(big(player.inertia_multiplier).subtract(1)).subtract(1);
+    return big(2).pow(big(player.inertia_multiplier).subtract(1)).subtract(1).mult(big(player.inertia_multiplier));
 }
 
 function inertia_eta() {
